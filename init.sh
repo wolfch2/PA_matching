a2bc80648e316bac56968d597d6d0ab4baf5e96d
fBedecarrats <f.bedecarrats@gmail.com>
2022-12-19 14:25:40 +0000

    Install Google CLI (required for Google earth engine API)

--- init.sh
+++ init.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
+# Install Google CLI
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates gnupg
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+sudo apt-get update && sudo apt-get install google-cloud-cli
+
+
 # Create variables
 WORK_DIR=/home/onyxia/work/PA_matching
 REPO_URL=https://${GIT_PERSONAL_ACCESS_TOKEN}@github.com/fBedecarrats/PA_matching.git # As initial
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 
 # Git
 git clone $REPO_URL $WORK_DIR
