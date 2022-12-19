09e9b51a2fff707b17bd9c9bb96ed2e60aaabb3f
Florent Bédécarrats <f.bedecarrats@gmail.com>
2022-12-19 14:39:07 +0100

    Create init.sh
    A file to reproduce the Wolf analysis from a reproducible cloud environment.

---
+++ init.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Create variables
+WORK_DIR=/home/onyxia/work/PA_matching
+REPO_URL=https://${GIT_PERSONAL_ACCESS_TOKEN}@github.com/fBedecarrats/PA_matching.git # As initial
+
+# Git
+git clone $REPO_URL $WORK_DIR
+chown -R onyxia:users $WORK_DIR
+
+# launch RStudio in the right project
+# Copied from InseeLab UtilitR
+    echo \
+    "
+    setHook('rstudio.sessionInit', function(newSession) {
+        if (newSession && !identical(getwd(), \"'$WORK_DIR'\"))
+        {
+            message('On charge directement le bon projet :-) ')
+            rstudioapi::openProject('$WORK_DIR')
+            # For a slick dark theme
+            rstudioapi::applyTheme('Merbivore')
+            # Console where it should be
+            rstudioapi::executeCommand('layoutConsoleOnRight')
+            }
+            }, action = 'append')
+            " >> /home/onyxia/work/.Rprofile
