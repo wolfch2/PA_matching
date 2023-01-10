ed0a7e7d5d7acfa4ce66dce6d683faa62e24b200
Florent Bédécarrats <f.bedecarrats@gmail.com>
2023-01-10 21:03:49 +0100

    Bug with google cloud sdk

--- init_v2.sh
+++ init_v2.sh
@@ -5,9 +5,9 @@ sudo apt-get update
 sudo apt-get install r-base
 
 # Install Google CLI
-sudo apt-get install apt-transport-https ca-certificates gnupg
-echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-sudo apt-get update && sudo apt-get install google-cloud-cli
+# sudo apt-get install apt-transport-https ca-certificates gnupg
+# echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+# sudo apt-get update && sudo apt-get install google-cloud-cli
 
 
 # Create variables
